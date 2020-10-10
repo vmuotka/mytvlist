15e6b6eb96fd5b0764055c0d492113748962f491
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2020-10-10 18:28:37 +0300

    tailwindcss setup and navbar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// project components
+import Navigation from './components/Navigation'
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App
