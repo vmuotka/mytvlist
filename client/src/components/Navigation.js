8d505c757f85f2608566cb8f6ec9adfa4ab00e5f
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2021-03-25 19:19:57 +0200

    navbar at the bottom of the screen on mobile

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -56,27 +56,28 @@ const Navigation = ({ children }) => {
 
   return (
     <>
-      <nav ref={navRef} className='flex items-center justify-between flex-wrap bg-pink-500 p-6 select-none'>
-        {/* Page Logo */}
-        <Link className='flex items-center flex-shrink-0 text-white mr-6' to='/'>
-          <span className='font-bold text-xl tracking-tight'>MyTvList</span>
-        </Link>
-        {/* Small Screen Width Hamburger Menu Button */}
-        <div className='block md:hidden'>
-          <button onClick={onMenuClick} className='flex items-center px-3 py-2 border rounded text-pink-200 border-pink-400 hover:text-white hover:border-white'>
-            <svg className='fill-current h-3 w-3' viewBox='0 0 20 20'><title>Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' /></svg>
-          </button>
+      <nav ref={navRef} className='fixed md:static bottom-0 flex-col-reverse md:flex-row w-full z-50 flex items-center justify-between flex-wrap bg-pink-500 p-6 select-none'>
+        <div className='flex'>
+          <Link className='flex items-center flex-shrink-0 text-white mr-6' to='/'>
+            <span className='font-bold text-xl tracking-tight'>MyTvList</span>
+          </Link>
+          {/* Small Screen Width Hamburger Menu Button */}
+          <div className='block md:hidden'>
+            <button onClick={onMenuClick} className='flex items-center px-3 py-2 border rounded text-pink-200 border-pink-400 hover:text-white hover:border-white focus:outline-none'>
+              <svg className='fill-current h-3 w-3' viewBox='0 0 20 20'><title>Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' /></svg>
+            </button>
+          </div>
         </div>
         {/* Navigation Links */}
         <div className={`${showNav ? 'block' : 'hidden'} w-full flex-grow md:flex md:items-center md:w-auto`} >
-          <div className='text-sm md:flex-grow'>
+          <div className='text-sm md:flex-grow grid grid-cols-2 md:flex text-center pt-4 pb-8 md:pt-0 md:pb-0 gap-6 md:gap-0'>
             {
               routes.map(route => (
-                route.hidden ? null : <Link to={route.href} key={route.title} className={`block text-base font-semibold mt-4 md:inline-block md:mt-0 ${location === route.href ? 'text-white' : 'text-pink-200'} hover:text-white mr-4`}>{route.title}</Link>
+                route.hidden ? null : <Link to={route.href} key={route.title} onClick={() => setShowNav(false)} className={`block text-base font-semibold md:inline-block md:mt-0 ${location === route.href ? 'text-white' : 'text-pink-200'} hover:text-white md:mr-4 focus:outline-none`}>{route.title}</Link>
               ))
             }
           </div>
-          <div>
+          <div className='border-t'>
             {
               authTokens ?
                 <button type='button' onClick={handleSignOut} value='Sign Out' className='inline-block text-xs font-bold px-3 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-pink-500 hover:bg-white mt-4 md:mt-0'>Sign Out</button> :
